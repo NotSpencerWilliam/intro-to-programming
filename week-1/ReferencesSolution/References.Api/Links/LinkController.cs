620aed5d059a3ffed252cdccaf575b2d649439fd
Spammons <ammons.spencer21@gmail.com>
2025-07-07 19:44:55 +0000

    Initial

---
+++ week-1/ReferencesSolution/References.Api/Links/LinkController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace References.Api.Links;
+
+public class LinkController : ControllerBase
+{
+    //Some code here that will get called when a GET /links is sent to this server
+    [HttpGet("/links")]
+    public async Task<ActionResult> GetAllLinksAsync(CancellationToken token)
+    {
+        return Ok();
+    }
+}
