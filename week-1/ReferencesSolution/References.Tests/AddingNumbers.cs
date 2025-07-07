3b6f7bf7abea4afbbf4e5e6df38da75f8929602f
Spammons <ammons.spencer21@gmail.com>
2025-07-07 20:20:39 +0000

    Fake Push

---
+++ week-1/ReferencesSolution/References.Tests/AddingNumbers.cs
@@ -0,0 +1,17 @@
+﻿namespace References.Tests
+{
+    public class AddingNumbers
+    {
+        [Fact]
+        public void CanAddTwoNumbers()
+        {
+            int a = 10;
+            int b = 20;
+            int answer;
+
+            answer = a + b;
+
+            Assert.Equal(31, answer);
+        }
+    }
+}
